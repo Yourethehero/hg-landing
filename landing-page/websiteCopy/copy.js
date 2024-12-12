f6e5bddf68850366da4c41638b78a015c41939c5
Yourethehero <121990741+Yourethehero@users.noreply.github.com>
2024-12-12 06:38:40 -0500

    Update copy.js added some text to the steps on the landing page

--- landing-page/websiteCopy/copy.js
+++ landing-page/websiteCopy/copy.js
@@ -1,10 +1,10 @@
 export const copy = {
     heroDescription: "Lorem ipsum dolor sit amet  , consectetur adipiscing elit. Quisque neque justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     howItWorks: {
-        step1: "How it works 1. Adding anything",
-        step2: "How it works 2",
-        step3: "How it works 3",
-        step4: "How it works 4"
+        step1: "Simply Choose Your Desired Style or Habit",
+        step2: "Upload Yours or Gift Recipient's Image",
+        step3: "Create Art And Choose Desired Product to Print",
+        step4: "See Live Mock Up of Product And Confirm Order"
     }
 
 }
